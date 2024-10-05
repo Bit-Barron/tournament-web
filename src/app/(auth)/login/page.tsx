0f99d183717a222913cdb53dc9a82a060985e5b7
bit-barron <bit.barron22@gmail.com>
2024-10-05 22:37:52 +0200

    clean up

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,6 +5,16 @@ import { authUser } from "@/server/auth/typebox";
 import Link from "next/link";
 import { useRouter } from "next/navigation";
 import { FormEvent, useState } from "react";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 interface LoginPageProps {}
 
@@ -22,41 +32,51 @@ export default function LoginPage(props: LoginPageProps) {
         username,
         password,
       })
-      .then((user) => (user ? router.push("/dashboard") : setStatus(user as string)))
+      .then((user) =>
+        user ? router.push("/dashboard") : setStatus(user as string),
+      )
       .catch((error) => setStatus(JSON.stringify(error)));
   };
 
   return (
     <form onSubmit={onSubmit} className="space-y-2">
-      <div className="flex flex-col border">
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={username}
-          minLength={authUser.properties.username.minLength}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </div>
-      <div className="flex flex-col border">
-        <label htmlFor="password">Password</label>
-        <input
-          id="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </div>
-      <div className="flex justify-between">
-        <button className="bg-red-600" type="submit">
-          Login
-        </button>
-
-        <Link href="/register">Register</Link>
-      </div>
-      {status && <div>{status}</div>}
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardDescription>
+            Enter your email below to login to your account
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="username">Username</Label>
+              <Input id="username" type="text" placeholder="admin" required />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+                <Link
+                  href="#"
+                  className="ml-auto inline-block text-sm underline"
+                >
+                  Forgot your password?
+                </Link>
+              </div>
+              <Input id="password" type="password" required />
+            </div>
+            <Button type="submit" className="w-full">
+              Login
+            </Button>
+          </div>
+          <div className="mt-4 text-center text-sm">
+            Don&apos;t have an account?
+            <Link href="#" className="underline">
+              Sign up
+            </Link>
+          </div>
+        </CardContent>
+      </Card>
     </form>
   );
 }
