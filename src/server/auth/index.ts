dfa412c0c4b399af8cb5af41650e4d7b0e016a9d
bit-barron <bit.barron22@gmail.com>
2024-10-05 22:23:24 +0200

    clean up

--- src/server/auth/index.ts
+++ src/server/auth/index.ts
@@ -4,23 +4,19 @@ import { serverEnv } from "@/utils/env/server";
 import { Elysia, InternalServerError } from "elysia";
 import { authUser } from "./typebox";
 
-/**
- * Authentication routes for user registration, login, and logout.
- * Needs to be combined at the `app/api/[[...route]]/route.ts` file with
- * Represents RPC client types based on input & output
- */
+
 export const authRoute = new Elysia({ prefix: "/auth" })
   .post(
     "/register",
     async (ctx) => {
       // Check if user already exists
-      const userExist = await prisma.user.findFirst({
+      const userExist = await prisma.admin.findFirst({
         where: { username: ctx.body.username.trim() },
       });
       if (userExist) throw new InternalServerError("User already exists");
 
       // Create new user
-      const user = await prisma.user.create({
+      const user = await prisma.admin.create({
         data: {
           username: ctx.body.username.trim(), // typesafe ctx.body based on `authUser` schema
           password: ctx.body.password.trim(), // typesafe ctx.body based on `authUser` schema
@@ -43,7 +39,7 @@ export const authRoute = new Elysia({ prefix: "/auth" })
     "/login",
     async (ctx) => {
       // Find user by username and password
-      const user = await prisma.user.findFirst({
+      const user = await prisma.admin.findFirst({
         where: {
           username: ctx.body.username.trim(), // typesafe ctx.body based on `authUser` schema
           password: ctx.body.password.trim(), // typesafe ctx.body based on `authUser` schema
