10bb9b0c3a8ad21b0dd4f35e306354479f3bf14a
bit-barron <bit.barron22@gmail.com>
2024-10-06 17:48:21 +0200

    clean up

--- src/store/DashboardStore.ts
+++ src/store/DashboardStore.ts
@@ -1,13 +1,5 @@
 import { proxy } from "valtio";
-import {
-  FiLayout,
-  FiUsers,
-  FiCalendar,
-  FiGitBranch,
-  FiBarChart2,
-  FiMessageSquare,
-  FiSettings,
-} from "react-icons/fi";
+import { FiLayout, FiUsers, FiGitBranch, FiSettings } from "react-icons/fi";
 import { IoIosTrophy } from "react-icons/io";
 
 export type DashboardStore = typeof DashboardStore;
@@ -17,10 +9,7 @@ export const DashboardStore = proxy({
     { name: "Dashboard", href: "/", icon: FiLayout },
     { name: "Tournaments", href: "/tournaments", icon: IoIosTrophy },
     { name: "Participants", href: "/participants", icon: FiUsers },
-    { name: "Schedule", href: "/schedule", icon: FiCalendar },
     { name: "Brackets", href: "/brackets", icon: FiGitBranch },
-    { name: "Reports", href: "/reports", icon: FiBarChart2 },
-    { name: "Messages", href: "/messages", icon: FiMessageSquare },
     { name: "Settings", href: "/settings", icon: FiSettings },
   ],
 });
